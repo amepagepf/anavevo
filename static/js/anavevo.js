adbe530b720c22d124eeffa696ad33664229a23d
amepagepf <alexandre@tahiti-ingenierie.pf>
2022-03-25 15:10:16 -1000

    # Revue fonction de recherche + requête insertion éclatement html

--- static/js/anavevo.js
+++ static/js/anavevo.js
@@ -30,6 +30,37 @@ function showArborescence(ieElement) {
 	}
 }
 
+function showSearchArborescence(ieElement) {
+	
+	var strClassName = ieElement.className;
+	var objParentElement = ieElement.parentElement.parentElement;
+	
+	var objUlElement = objParentElement.lastElementChild;
+	
+	while (objUlElement != null && objUlElement.nodeName == "UL") {
+		
+		var objSubElement = objUlElement.firstElementChild;
+		
+		var strObjSubElementClassName = objSubElement.className;
+			
+		var intIndexOf = 0;
+		var strNewClassName = null;
+		// Il faut show la sous-classe
+		if (strClassName == "arrow-right") {
+			intIndexOf = strObjSubElementClassName.indexOf("hide");
+			strNewClassName= strObjSubElementClassName.substring(0, intIndexOf) + "show";
+			ieElement.className = ieElement.className.replace(strClassName, "arrow-down");
+			objSubElement.className = objSubElement.className.replace(strObjSubElementClassName, strNewClassName);		
+		} else {
+			intIndexOf = strObjSubElementClassName.indexOf("show");
+			strNewClassName= strObjSubElementClassName.substring(0, intIndexOf) + "hide";
+			ieElement.className = ieElement.className.replace(strClassName, "arrow-right");
+			objSubElement.className = objSubElement.className.replace(strObjSubElementClassName, strNewClassName);		
+		}
+		
+		objUlElement = objUlElement.nextElementSibling;
+	}
+}
 
 function toggleEye(ieElement) {
 
